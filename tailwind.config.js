ed3c95f68f07a0191d94ee48d0726c10aa91b188
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-12-17 14:08:56 +0000

    fix head links in html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 module.exports = {
     content: [
+        "./public/**/*.html",
         "./src/**/*.{html,ts,js}",   
         "./*.{html,ts,js}",          
     ],
